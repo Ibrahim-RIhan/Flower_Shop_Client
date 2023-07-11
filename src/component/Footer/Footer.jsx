a706c6a6e1c05c1e01427618240af994a7ebbefa
rihan010 <ibrhimrihan000@gmail.comm>
2023-07-11 23:02:31 +0600

    footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="text-center leading-relaxed p-20 bg-stone-800 text-white">
+      <h1 className="text-3xl font-bold">Flower Shop</h1>
+      <p className="text-gray-300 my-5">
+        It is a long established fact that a reader will be distracted by the
+        readable <br /> content of a page when looking at its layout.
+      </p>
+      <p className="text-gray-500 my-3">
+        @2020-2023, FlowerShop.com. All rights reserved.{" "}
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
